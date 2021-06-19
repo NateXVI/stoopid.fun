40222eda88895e72872824b9fb91aaff34e02e44
NateXVI <natediven1@gmail.com>
2021-06-19 07:59:43 -0600

    added random button

---
+++ random.js
@@ -0,0 +1,13 @@
+const sites = [
+	'https://canvas.stoopid.fun',
+	'https://flappy.stoopid.fun',
+	'https://dvd.stoopid.fun',
+];
+
+const button = document.querySelector('button.random');
+
+function randomSite() {
+	console.log('asfdasdf');
+	const site = sites[Math.floor(Math.random() * sites.length)];
+	window.location = site;
+}
